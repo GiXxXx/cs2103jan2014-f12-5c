ce3c8cb5c2c244b51c2bbb7ee286c130ae400b5e
CY <CY@localhost>
2014-03-24 21:17:52 +0800

    New pattern: command pattern

---
+++ VeriTask/VeriTask/VeriTask.h
@@ -0,0 +1,27 @@
+#ifndef HEADER_VERITASK_
+#define HEADER_VERITASK_
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include "Command.h"
+#include "AddTask.h"
+#include "DeleteTask.h"
+#include "EditTask.h"
+#include "SearchTask.h"
+#include "MarkTask.h"
+
+using namespace std;
+
+class VeriTask {
+private:
+	vector<Command*> commandList;
+	DataStorage _dataStorage;
+	vector<DataStorage> undoStorage;
+public:
+	VeriTask();
+	~VeriTask();
+	void pushCommand(string, Identifier); 
+	
+};
+#endif
