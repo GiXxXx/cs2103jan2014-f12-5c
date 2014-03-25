2a7ab7fea7ed1b4e269e544f12f7d883f23b912e
gap jia cheng <gap.jia.cheng@hotmail.com>
2014-03-25 18:12:44 +0800

    identifier with new pattern

--- VeriTask/VeriTask/SearchTask.cpp
+++ VeriTask/VeriTask/SearchTask.cpp
@@ -2,7 +2,7 @@
 
 void SearchTask::executeCommand(Identifier infoIdentifier, DataStorage &_dataStorage) {
 	bool isFound;
-	string keyword=infoIdentifier.getKeyword();
+	string keyword=infoIdentifier.GetKeyword();
 	isFound = _dataStorage.searchData(keyword);
 	if (!isFound) {
 		cout << "Not Found!\n";
