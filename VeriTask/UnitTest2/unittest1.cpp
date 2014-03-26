b30dfb10a55d65330f80177c2edfffe1bf30cb7e
Yu Jia <soilleur.tamaka@gmail.com>
2014-03-26 17:26:43 +0800

    add unit test

---
+++ VeriTask/UnitTest2/unittest1.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "AddTask.h"
+#include "Command.h"
+#include "CommandGetter.h"
+#include "DataStorage.h"
+#include "DateGetter.h"
+#include "DeleteTask.h"
+#include "EditTask.h"
+#include "EndTimeGetter.h"
+#include "EventGetter.h"
+#include "Identifier.h"
+#include "KeywordGetter.h"
+#include "MarkTask.h"
+#include "resource.h"
+#include "SearchTask.h"
+#include "StartTimeGetter.h"
+#include "StatusGetter.h"
+#include "Task.h"
+#include "TaskNumGetter.h"
+#include "TextUI.h"
+#include "Tokenizer.h"
+#include "VeriTask.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+		}
+
+	};
+}
