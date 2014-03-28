3c259934448aa83b98cd06aceada1a2ab31481e2
CY <CY@localhost>
2014-03-28 11:11:17 +0800

    V0.2

--- VeriTask/VeriTask/StatusGetter.cpp
+++ VeriTask/VeriTask/StatusGetter.cpp
@@ -8,27 +8,16 @@ StatusGetter::StatusGetter(string& uncategorizedInfo, string Command):Tokenizer(
 StatusGetter::~StatusGetter(){
 }
 
-/*string StatusGetter::Tokenize(){
-	string status = NoStatus;
-	unsigned int position = (*uncategorizedInfo).find_last_not_of(space);
-
-	if(command == Mark){
-		if(*uncategorizedInfo == Done || *uncategorizedInfo == UnDone || *uncategorizedInfo == CannotBeDone) {
-			status = (*uncategorizedInfo);.substr(start, position + OneUnit);
-		} 
-	}
-
-	return status;
-}*/
-
 string StatusGetter::Tokenize(){
 	string status = NoStatus;
-
+	unsigned int position = (*uncategorizedInfo).find_last_not_of(space);
+	string temp = (*uncategorizedInfo).substr(start, position + OneUnit);
 	if(command == Mark){
-		if(*uncategorizedInfo == Done || *uncategorizedInfo == UnDone || *uncategorizedInfo == CannotBeDone) {
-			status = *uncategorizedInfo;
+		if(temp == Done || temp == UnDone || temp == CannotBeDone) {
+			status = temp;
 		} 
 	}
 
 	return status;
 }
+
