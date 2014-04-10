fc2068b461189ded5cba187e72ff179dedbc9cc7
gap jia cheng <gap.jia.cheng@hotmail.com>
2014-04-10 22:32:12 +0800

    revert

--- VeriTask/VeriTask/TimeGetter.cpp
+++ VeriTask/VeriTask/TimeGetter.cpp
@@ -53,78 +53,85 @@ string TimeGetter::getTime(string Input, string keyword){
 	do{
 		position = duplicate.find(keyword, startPos);
 
-		if(position != string::npos){
+		if (position != string::npos){
 			tempTime = duplicate.substr(position + keyword.size());
 			sizeOne = tempTime.find_first_of(space);
-			sizeTwo = tempTime.find_first_of(space, sizeOne + OneUnit); 
-					
+			sizeTwo = tempTime.find_first_of(space, sizeOne + OneUnit);
+
 			checker = tempTime.substr(sizeOne + OneUnit, sizeTwo - sizeOne - OneUnit);
 
-			if(checker == pmOne || checker == pmTwo || checker == pmThree){
+			if (checker == pmOne || checker == pmTwo || checker == pmThree){
 				hourStandard = TwoUnit;
 			}
 
-			if(checker == amOne || checker == amTwo || checker == amThree){
+			if (checker == amOne || checker == amTwo || checker == amThree){
 				hourStandard = OneUnit;
 			}
 
 			tempTime = tempTime.substr(start, sizeOne);
 
-			string apm[SixUnit] = {amOne, amTwo, amThree, pmOne, pmTwo, pmThree};
-	    	unsigned int indicator = zero;
+			string apm[SixUnit] = { amOne, amTwo, amThree, pmOne, pmTwo, pmThree };
+			unsigned int indicator = zero;
 
-			while(!isNumber(tempTime) && indicator < SixUnit){
-    			unsigned int newPosition = tempTime.find(apm[indicator]);
+			while (!isNumber(tempTime) && indicator < SixUnit){
+				unsigned int newPosition = tempTime.find(apm[indicator]);
 
-	   			if(newPosition != string::npos){
+				if (newPosition != string::npos){
 					string temp = tempTime.substr(start, newPosition);
-				
-					if(isNumber(temp)){
+
+					temp = convertToTime(temp);
+
+					if (isNumber(temp)){
 						tempTime = temp;
-						if(indicator < ThreeUnit){
-			    			hourStandard = OneUnit;
-		    			}
-		    			else{
-		    				hourStandard = TwoUnit;
-			    			}
-
-							sizeTwo = sizeOne;
-							break;
+						if (indicator < ThreeUnit){
+							hourStandard = OneUnit;
 						}
+						else{
+							hourStandard = TwoUnit;
+						}
+
+						sizeTwo = sizeOne;
+						break;
 					}
-
-					indicator++;
 				}
 
-				if(hourStandard == zero){
-					sizeTwo = sizeOne;
-				}
+				indicator++;
+			}
 
+			if (hourStandard == zero){
+				sizeTwo = sizeOne;
 				tempTime = convertToTime(tempTime);
 
-				if(isNumber(tempTime)){
+				if (isNumber(tempTime)){
 					Time = tempTime;
+					chopInfo((*uncategorizedInfo), position, sizeTwo + keyword.size());
+					break;
+				}
+			}
 
-					if(hourStandard == OneUnit || hourStandard == zero){
-	   	    			chopInfo((*uncategorizedInfo), position, sizeTwo + keyword.size());
-	    				break;
-	   	    		}
+			if (isNumber(tempTime)){
+				Time = tempTime;
 
-	   	    		if(hourStandard == TwoUnit){
-		        		int TimeNumber = std::stoi(tempTime);
-	    	   			TimeNumber = hourAdder + TimeNumber;
-		      			stringstream in;
-	    	   			in << TimeNumber;
-	    	   			Time = in.str();
-	    	   			chopInfo((*uncategorizedInfo), position, sizeTwo + keyword.size());
-	    				break;
-	    	   		}
-	    	  	}
-	    	}
+				if (hourStandard == OneUnit || hourStandard == zero){
+					chopInfo((*uncategorizedInfo), position, sizeTwo + keyword.size());
+					break;
+				}
 
-	    	startPos = position + OneUnit;
-		
-		}while(position != string::npos);	
+				if (hourStandard == TwoUnit){
+					int TimeNumber = std::stoi(tempTime);
+					TimeNumber = hourAdder + TimeNumber;
+					stringstream in;
+					in << TimeNumber;
+					Time = in.str();
+					chopInfo((*uncategorizedInfo), position, sizeTwo + keyword.size());
+					break;
+				}
+			}
+		}
+
+		startPos = position + OneUnit;
+
+	} while (position != string::npos);
 
 	return Time;
 }
@@ -208,37 +215,38 @@ string TimeGetter::getTimeFromMorning(string Input, string keyword){
 }
 
 string TimeGetter::convertToTime(string tempTime){
-	if(tempTime.size() >= FourUnit){
-		if((tempTime.substr(OneUnit,OneUnit)) == colon || tempTime.substr(OneUnit,OneUnit) == dot){
-			tempTime = tempTime.substr(start, OneUnit) + tempTime.substr(start + TwoUnit, TwoUnit);
-  		}
+	int position = tempTime.find(colon);
+	string hour = emptyString, minute = emptyString;
 
-   	   	if((tempTime.substr(TwoUnit,OneUnit)) == colon || tempTime.substr(TwoUnit,OneUnit) == dot){
-	   		tempTime = tempTime.substr(start, TwoUnit) + tempTime.substr(start + ThreeUnit, TwoUnit);
-			}
+	if (position == string::npos) {
+		position = tempTime.find(dot);
 	}
-				
-   if(isNumber(tempTime)){
-	   if(tempTime.size() == OneUnit){
-		   tempTime = Zero + tempTime + Zero + Zero;
-    	}
-	   else if(tempTime.size() == TwoUnit){
-		   tempTime = tempTime + Zero + Zero;
-	   }
-	   else if(tempTime.size() == ThreeUnit){
-		   tempTime = Zero + tempTime + Zero;
-	   }
-   }
 
-   return tempTime;
-}
+	if (position != string::npos) {
+		hour = tempTime.substr(start, position - start);
+		minute = tempTime.substr(position + OneUnit);
+	}
 
-void Tokenizer::ChangeDoubleDigit(string &Number){
-	if(isNumber(Number)){
-		if(std::stoi(Number) < 10 && Number.size() == OneUnit){
-			Number = Zero + Number;
+	if (isNumber(hour) && isNumber(minute) && hour != emptyString && minute != emptyString){
+		if (hour.size() == OneUnit){
+			hour = Zero + hour;
+		}
+
+		if (minute.size() == OneUnit){
+			minute = minute + Zero;
+		}
+
+		tempTime = hour + minute;
+	}
+
+	if (isNumber(tempTime) && tempTime.size() <= TwoUnit){
+		if (tempTime.size() == OneUnit){
+			tempTime = Zero + tempTime + Zero + Zero;
+		}
+		else{
+			tempTime = tempTime + Zero + Zero;
 		}
 	}
 
-	return;
+	return tempTime;
 }
