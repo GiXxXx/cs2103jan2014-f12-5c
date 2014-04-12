0ed048695ba65a3af169c2d7bb64c2251bafa3f3
CY <CY@localhost>
2014-04-12 13:08:07 +0800

    Logic Refactored

--- VeriTask/VeriTask/Task.cpp
+++ VeriTask/VeriTask/Task.cpp
@@ -1,3 +1,5 @@
+//@author A0101039X
+
 #include "Task.h"
 
 Task::Task(string event, string date, string startTime, string endTime, string status, int index) {
@@ -7,108 +9,59 @@ Task::Task(string event, string date, string startTime, string endTime, string s
 	_endTime = endTime;
 	_status = status;
 	_index = index;
-	unsigned long long int date_Int;
-	unsigned long long int startTime_Int;
-	unsigned long long int endTime_Int;
 
-	if(date == "        "){
-		date_Int = 99999;
-	}
-	else{
-		date_Int = std::stoi(_date.substr(2,6));
-	}
-	if(startTime == "    "){
-		startTime_Int = 9999;
-	}
-	else{
-		startTime_Int = std::stoi(_startTime);
-	}
-	if(endTime == "    "){
-		endTime_Int = 9999;
-	}
-	else{
-		endTime_Int = std::stoi(_endTime);
-	}
-
-	struct tm timeNow;
-	time_t localTime = time(NULL);
-
-	localtime_s(&timeNow, &localTime);
-
-	int localYear = timeNow.tm_year + 1900;
-	int localMonth = timeNow.tm_mon + 1;
-	int localDate = timeNow.tm_mday;
-	int localHour = timeNow.tm_hour;
-	int localMin = timeNow.tm_min;
-	
-	if ((date != "        ") && (status != "done")) {
-	if (localYear*10000+localMonth*100+localDate > stoi(date)) {
-		_status = "overdue";
-	} else if (localYear*10000+localMonth*100+localDate == stoi(date)) {
-		if ((startTime != "    ") && (endTime == "    ")) {
-			if ((localHour*100+localMin) > stoi(startTime)) {
-				_status = "overdue";
-			}
-		}
-		if ((startTime != "    ") && (endTime != "    ")) {
-			if ((localHour*100+localMin) > stoi(endTime)) {
-				_status = "overdue";
-			}
-		}
-	}
-	}
-
-	_ID = date_Int * 10000000000000 + startTime_Int * 1000000000  + endTime_Int * 100000 + index;
+	_ID = _calculateTaskID( _date, _startTime, _endTime, _index);
+    _checkOverdue(_date, _startTime, _endTime, _status);
 }
 
-string Task::getEvent() {
-	return _event;
+inline string Task::getEvent() { 
+	return _event; 
 }
 
-string Task::getDate() {
-	return _date;
+inline string Task::getDate() { 
+	return _date; 
 }
 
-string Task::getStartTime() {
+inline string Task::getStartTime() {
 	return _startTime;
 }
 
-string Task::getEndTime() {
+inline string Task::getEndTime() {
 	return _endTime;
 }
 
-string Task::getStatus() {
+inline string Task::getStatus() {
 	return _status;
 }
 
-unsigned long long Task::getID() {
+inline unsigned long long Task::getID() {
 	return _ID;
 }
 
-int Task::getIndex() {
+inline int Task::getIndex() {
 	return _index;
 }
 
-void Task::setEvent(string event) {
+inline void Task::setEvent(string event) {
 	_event = event;
 }
 
-void Task::setDate(string date) {
+inline void Task::setDate(string date) {
 	_date = date;
 }
 
-void Task::setStartTime(string startTime) {
+inline void Task::setStartTime(string startTime) {
 	_startTime = startTime;
 }
 
-void Task::setEndTime(string endTime) {
+inline void Task::setEndTime(string endTime) {
 	_endTime = endTime;
 }
 
-void Task::setStatus(string status) {
+inline void Task::setStatus(string status) {
 	_status = status;
 }
 
-void Task::setID(unsigned long long ID) {
+inline void Task::setID(unsigned long long ID) {
 	_ID = ID;
 }
--- VeriTask/VeriTask/Task.h
+++ VeriTask/VeriTask/Task.h
@@ -1,13 +1,39 @@
-#ifndef HEADER_TASK_
-#define HEADER_TASK_
-#include <iostream>
-#include <vector>
+//@author A0101039X
+
+/**
+* This is a class that records all necessary information for a task.
+* It alos contains methods to modify and retrieve the information.
+* Sample usage:
+* Task task;
+* task.retrieveTaskID();
+*/
+
 #include <string>
-#include <sstream>
-#include <algorithm>
 #include <ctime>
 
 using namespace std;
+
+const string EMPTY_DATE = "        ";
+const string EMPTY_TIME = "    ";
+
+const int TWO_POSITION = 2;
+const int SIX_POSITION = 6;
+
+const int LARGE_DATE = 999999;
+const int LARGE_TIME = 9999;
+
+const unsigned long long int THIRTEEN_ZEROS = 10000000000000;
+const unsigned long long int NINE_ZEROS = 1000000000;
+const unsigned long long int FIVE_ZEROS = 100000;
+
+const int FOUR_ZEROS = 10000;
+const int TWO_ZEROS = 100;
+
+const int ONE_NINE_ZERO_ZERO = 1900;
+const int ONE = 1;
+
+const string DONE = "done";
+const string OVERDUE = "overdue";
 
 class Task {
 
@@ -17,25 +43,102 @@ private:
 	string _startTime;
 	string _endTime;
 	string _status;
+	//this is the running index; it will increase by one each time a new task is created
+	//so that tasks with same date and time will have different IDs based on sequence of
+	//entering
 	int _index;
 	unsigned long long _ID;
 
+	//this private function is used calculate the task ID for each task.
+	//Pre-req: date, startTime, endTime, index
+	//Post-req: task ID
+	unsigned long long int _calculateTaskID (string date, string startTime, string endTime, int index) {
+		unsigned long long ID;
+		unsigned long long int date_Int;
+		unsigned long long int startTime_Int;
+		unsigned long long int endTime_Int;
+		
+		//if there is no date, set it to be 999999, so that it will be sorted
+		//to the back as floating task.
+		if (date == EMPTY_DATE) {
+			date_Int = LARGE_DATE;
+		} else {
+			date_Int = stoi(date.substr(TWO_POSITION, SIX_POSITION));
+		}
+		
+		//if there is no time, set it to be 9999
+		if (startTime == EMPTY_TIME) {
+			startTime_Int = LARGE_TIME;
+		} else {
+			startTime_Int = stoi(startTime);
+		}
+	
+		if (endTime == EMPTY_TIME) {
+			endTime_Int = LARGE_TIME;
+		} else {
+			endTime_Int = stoi(endTime);
+		}
+
+		//ID is made of 19 digits. The first 6 digits are in the form YYMMDD, next 8 digits
+		//are start time followed by end time, the last 5 digits are for running index.
+		ID = date_Int * THIRTEEN_ZEROS + startTime_Int * NINE_ZEROS  + endTime_Int * FIVE_ZEROS + index;
+		return ID;
+	}
+
+	//this private function is used to check whether a task is overdue
+	//Pre-req: date, start time, end time, status
+	//Post-req: nil
+	void _checkOverdue (string date, string startTime, string endTime, string status) {
+		struct tm timeNow;
+		time_t localTime = time(NULL);
+		localtime_s(&timeNow, &localTime);
+
+		//get local time
+		int localYear = timeNow.tm_year + ONE_NINE_ZERO_ZERO;
+		int localMonth = timeNow.tm_mon + ONE;
+		int localDate = timeNow.tm_mday;
+		int localHour = timeNow.tm_hour;
+		int localMin = timeNow.tm_min;
+	    
+		//check whether a task is overdue
+		//if a task is not marked as done and has passed deadline, then it's overdue
+		if ((date != EMPTY_DATE) && (status != DONE)) {
+			if (localYear * FOUR_ZEROS + localMonth * TWO_ZEROS + localDate > stoi(date)) {
+				status = OVERDUE;
+			} else if (localYear * FOUR_ZEROS + localMonth * TWO_ZEROS + localDate == stoi(date)) {
+				//deadline task is overdue if time now is later than deadline
+				if ((startTime != EMPTY_TIME) && (endTime == EMPTY_TIME)) {
+					if ((localHour * TWO_ZEROS + localMin) > stoi(startTime)) {
+					status = OVERDUE;
+					}
+				}
+				
+				//duration task is marked as overdue if time now is later than end time
+				//of the task
+				if ((startTime != EMPTY_TIME) && (endTime != EMPTY_TIME)) {
+					if ((localHour * 100 + localMin) > stoi(endTime)) {
+						status = OVERDUE;
+					}
+				}
+			}
+		}
+	}
+
 public:
 	Task(string event, string date, string startTime, string endTime, string status, int index);
-	string getEvent();
-	string getDate();
-	string getStartTime();
-	string getEndTime();
-	string getStatus();
-	unsigned long long getID();
-	int getIndex();
+	
+	inline string getEvent();
+	inline string getDate();
+	inline string getStartTime();
+	inline string getEndTime();
+	inline string getStatus();
+	inline unsigned long long getID();
+	inline int getIndex();
 
-	void setEvent(string event);
-	void setDate(string date);
-	void setStartTime(string startTime);
-	void setEndTime(string endTime);
-	void setStatus(string status);
-	void setID(unsigned long long ID);
+	inline void setEvent(string event);
+	inline void setDate(string date);
+	inline void setStartTime(string startTime);
+	inline void setEndTime(string endTime);
+	inline void setStatus(string status);
+	inline void setID(unsigned long long ID);
 };
-
-#endif
